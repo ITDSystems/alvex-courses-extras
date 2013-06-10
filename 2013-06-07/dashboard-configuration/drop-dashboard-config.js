91bf34028852871af2e8c5df35862795791bf0de
Alexey Vasyukov <vasyukov@gmail.com>
2013-06-10 12:26:36 +0400

    Script to drop user dashboard configuration - credits to @fufler

---
+++ 2013-06-07/dashboard-configuration/drop-dashboard-config.js
@@ -0,0 +1,21 @@
+/* This script is meant to be run in JavaScript Console */
+
+// We will not drop configuration for these users
+var usersToIgnore = ['admin', 'crazyEd'];
+
+// Get dashboard configs for all users
+for each (var node in search.xpathSearch('/app:company_home/st:sites/cm:surf-config/cm:pages/cm:user//.'))
+{
+	// Get name of current user
+	var username = String(node.name);
+	// If the user is not in ignore list
+	if (usersToIgnore.indexOf(username) == -1)
+	{
+		print("Dropping configuration for " + username);
+		// Drop configuration completely for this user
+		node.remove();
+		for each (var _node in companyhome.childrenByXPath('st:sites/cm:surf-config/cm:components//.'))
+			if (_node.name.indexOf('user~'+username+'~dashboard.xml') != -1)
+				_node.remove();
+	}
+}
