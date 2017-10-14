71e846d17ba3e427bb5f1f2544dd185704a476fc
Oksana Kurysheva <okurysheva@itdhq.com>
2017-10-14 13:56:36 +0300

    export-import groups membership script

---
+++ 2017-10-14/export-import-groups/exportGroupMembers.js
@@ -0,0 +1,39 @@
+/* 
+ * This script creates another JS script that adds users to groups on the fresh Alfresco like it's on the old Alfresco instance.
+ * Example: you have groups A and B in Alfresco. There are User1 and User2 in A group, User3 is in B group. 
+ * Execute the following code in JavaScript Console, then grab createGroups.js in the repository. 
+ * On new Alfresco instance create users first (using http://alfrescoblog.com/2014/09/04/alfresco-export-users-from-one-server-to-the-other/ for example).
+ * Then execute createGroups.js on the new Alfresco. It will add users User1 and User2 to group A and User3 to group B. 
+ * Bonus: if you want also to create groups on the new Alfresco instance automatically, set flag "createIfNotExist" to true. If you want to create groups manually or by another tools, set this flag to false. In this case users will not be added to the groups that are not exist.
+ */
+
+var createIfNotExist = false;
+
+var paging = utils.createPaging(-1, 0);
+var groupCollection = groups.getGroups("", paging);
+
+var output = "";
+for (var i = 0; i < groupCollection.length; i++) {
+  var group = groupCollection[i];
+  var node = people.getGroup("GROUP_" + group.shortName);
+  if (createIfNotExist == true) {
+    output += ("var group = people.getGroup('GROUP_" + group.shortName + "');\n");
+    output += ("if (!group) {\n");
+    output += (" group = people.createGroup('" + group.shortName + "');\n");
+    output += ("}\n");
+  }
+  var members = people.getMembers(node);
+  if (members.length != 0) {
+    output += ("var group = people.getGroup('GROUP_" + group.shortName + "');\n");
+    output += ("if (group) {\n");
+    for (var j = 0; j < members.length; j++) {
+      output += (" var user = people.getPerson('" + members[j].properties["cm:userName"] + "');\n");
+      output += (" try {\n  people.addAuthority(group, user);\n }\n catch (ex)\n {}\n");
+    }
+    output += ("}\n");
+  }
+}
+
+var article = companyhome.createNode("createGroups.js", "cm:content");
+article.content = output;
+article.save();
